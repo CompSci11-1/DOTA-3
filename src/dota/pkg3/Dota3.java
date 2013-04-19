1d13a23053eef0802c2f81dbaaa4c58e0df81f86
evan ford <evan.ford@brentwood.bc.ca>
2013-04-19 09:04:46 -0700

    menu

--- src/dota/pkg3/Dota3.java
+++ src/dota/pkg3/Dota3.java
@@ -9,6 +9,7 @@ import environment.Environment;
 import image.ResourceTools;
 import java.awt.Dimension;
 import java.awt.Image;
+import javax.swing.JFrame;
 
 /**
  *
@@ -20,17 +21,30 @@ public class Dota3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        startDotaEnvironment();
+//        startDotaEnvironment();
+        newWindow();
+        
     }
     
-    private static void startDotaEnvironment(){
-        String[] args = new String[0];
-        String appName = "Dota 3";
-        Dimension appSize = new Dimension(1200, 700);
-//        Dimension appSize = new Dimension(900, 500);
-//        Image background = ResourceTools.loadImageFromResource("");
+//    private static void startDotaEnvironment(){
+//        String[] args = new String[0];
+//        String appName = "Dota 3";
+//        Dimension appSize = new Dimension(1200, 700);
+////        Dimension appSize = new Dimension(900, 500);
+////        Image background = ResourceTools.loadImageFromResource("");
+//
+//        Environment environment = new DotaEnvironment();
+//        ApplicationStarter.run(args, appName, appSize, environment);
+//    }
+     private static void newWindow() {
+        JFrame frame = new JFrame("Menu");
 
-        Environment environment = new DotaEnvironment();
-        ApplicationStarter.run(args, appName, appSize, environment);
+        MainMenu cp = new MainMenu();
+        frame.add(cp);
+        frame.setAlwaysOnTop(true);
+
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(new Dimension(400, 350));
+        frame.setVisible(true);
     }
 }
